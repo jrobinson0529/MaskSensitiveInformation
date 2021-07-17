9fad6c40126bba061bfaa2682b86eca67dbafcbb
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-07-17 11:34:19 -0500

    Add project files.

---
+++ MaskSensitiveInformation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MaskSensitiveInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your password");
+            var value = Console.ReadLine();
+            var numberToRemove = value.Length - 4;
+            var fourLetters = value.Remove(0, numberToRemove);
+            var remainingLetters = value.Remove(numberToRemove);
+            var maskedLetters = new StringBuilder();
+            for (var i=0; i < remainingLetters.Length; i++)
+            {
+                maskedLetters.Append('*');
+            }
+            Console.WriteLine(maskedLetters + fourLetters);
+        }
+    }
+}
